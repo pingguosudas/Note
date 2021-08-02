79d4475404490b284b3a8241e00d05f01a03188b
pingguosudas <88297088+pingguosudas@users.noreply.github.com>
2021-08-02 09:44:10 +0800

    Add files via upload

---
+++ Homework01.java
@@ -0,0 +1,7 @@
+
+public class Homework01 {
+	public static void main (String[]args){
+		//±‡–¥hello,world≥Ã–Ú
+		System.out.println("hello,world");	
+	}
+}
---
+++ Homework02.java
@@ -0,0 +1,8 @@
+
+//将个人的基本信息（姓名、性别、籍贯、住址）打印到控制台上输出。各条信息分别占一行
+public class Homework02 {
+	public static void main (String[]args){
+		//考察对转义字符的使用
+		System.out.println("姓名\t性别\t籍贯\t住址\n石介\t男\t湖北\t红安");	
+	}
+}
